8bd1921e79edc0db6f0a029e476533d209c9278e
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2024-10-01 18:06:04 +0300

    add new models: Kitten, Breed

--- backend/kittens/admin.py
+++ backend/kittens/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from kittens.models import Breed, Kitten
+
+
+@admin.register(Kitten)
+class KittenAdmin(admin.ModelAdmin):
+    list_display = ['color', 'age', 'description', 'breed']
+    list_filter = ['breed']
+
+@admin.register(Breed)
+class BreedAdmin(admin.ModelAdmin):
+    list_display = ['title']
--- backend/kittens/models.py
+++ backend/kittens/models.py
@@ -1,3 +1,22 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Kitten(models.Model):
+    color = models.CharField(max_length=50)
+    age = models.IntegerField(validators=[MinValueValidator(1),
+                                          MaxValueValidator(360)])
+    description = models.TextField()
+    breed = models.ForeignKey('Breed',
+                              on_delete=models.PROTECT,
+                              related_name='kittens')
+
+    def __str__(self):
+        return f'kitten, color: {self.color}, age: {self.age} months'
+
+
+class Breed(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return f'breed: {self.title}'
