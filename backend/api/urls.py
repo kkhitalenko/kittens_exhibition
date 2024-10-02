9d18e9500ae9fd095f6c567ebee4b2534b26e938
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2024-10-02 16:25:59 +0300

    add new urls

---
+++ backend/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from api.views import BreedList, KittenViewSet
+
+
+app_name = 'api'
+
+router = DefaultRouter()
+router.register(r'kittens', KittenViewSet, basename='kittens')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('breeds/', BreedList.as_view()),
+]
