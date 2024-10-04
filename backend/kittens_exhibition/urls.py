1a4a7aa0bd8b082e46ccefb157659e0602513dda
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2024-10-04 18:08:53 +0300

    refactor: set permission for swagger

--- backend/kittens_exhibition/urls.py
+++ backend/kittens_exhibition/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
 
 
 schema_view = get_schema_view(
@@ -11,6 +12,7 @@ schema_view = get_schema_view(
       description="Документация для онлайн выставки котят",
    ),
    public=True,
+   permission_classes=(AllowAny,),
 )
 
 urlpatterns = [
